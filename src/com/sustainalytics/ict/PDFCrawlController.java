183bdcce782bb24d1281cc60f581892d6854e0c8
Rushdi Shams <rushdecoder@gmail.com>
2015-04-21 12:36:49 -0400

    v1.1

--- src/com/sustainalytics/ict/PDFCrawlController.java
+++ src/com/sustainalytics/ict/PDFCrawlController.java
@@ -1,7 +1,11 @@
 package com.sustainalytics.ict;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+
+import org.apache.commons.io.FileUtils;
 
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
@@ -9,45 +13,73 @@ import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
+/**
+ * The controller class for the crawler. It has a main method that reads all the company URLs
+ * from a source text file and crawls them one by one. The method configures the crawler parameters. Also
+ * the method keeps track of the time taken by the crawler
+ * @author Sustainalytics
+ * @version 1.1 April 20 2015
+ *
+ */
+
 public class PDFCrawlController {
+
 	public static void main(String[] args) throws Exception {
-		Instant start = Instant.now();
-		
+		/*
+		 * Configuration storage, no. of crawlers to start with, storage folder for the downloaded PDFs
+		 */
 		String rootFolder = "crawler4jStorage";
 		int numberOfCrawlers = 10;
-		String storageFolder = "secondstorage";
+		String storageFolder = "pdfstorage";
 
-		CrawlConfig config = new CrawlConfig();
-		config.setCrawlStorageFolder(rootFolder);
-		/*
-		 * Since images are binary content, we need to set this parameter to
-		 * true to make sure they are included in the crawl.
-		 */
-		config.setIncludeBinaryContentInCrawling(true);
-		//My configurations-->
-		config.setPolitenessDelay(1);
-		config.setMaxDepthOfCrawling(10);
-		config.setMaxPagesToFetch(-1);
-		//config.setIncludeHttpsPages(true);
-		config.setMaxDownloadSize(20000000);
-		
-		
-		//<--My configuration ends
 
-		String domain = "http://www.sustainalytics.com/";
+		//Apache Commons IO usage for getting the urls from a source text file --->
+		File urlFile = new File("input/urls-test.txt");
+		String url = "";
+		try {
+			url = FileUtils.readFileToString(urlFile);
 
-		PageFetcher pageFetcher = new PageFetcher(config);
-		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+		} catch (IOException e) {
+			System.out
+			.println("Error to read urls from text file");
+			e.printStackTrace();
+		}
 
-		controller.setCustomData(domain);
-		controller.addSeed(domain);
+		String[] urls = url.trim().split("\n");
+		//<--- url reading done
+		Instant start = Instant.now(); //start timer
 
-		PDFCrawler.configure(domain, storageFolder);
+		//for each url--->
+		for (String domain: urls){	
 
-		controller.start(PDFCrawler.class, numberOfCrawlers);
+			//Let's configure the crawler--->
+			CrawlConfig config = new CrawlConfig();
+			config.setCrawlStorageFolder(rootFolder); //configuration folder
+			config.setIncludeBinaryContentInCrawling(true); //PDFs are binary contents
+			config.setPolitenessDelay(1); //We are very unpolite. In miliseconds
+			config.setMaxDepthOfCrawling(10); //Infinite depth
+			config.setMaxPagesToFetch(-1); //Infite fetching of pges
+			config.setIncludeHttpsPages(true); //We move to secured http as well
+			config.setMaxDownloadSize(20000000); //Quite a huge file size. In bytes
+
+			PageFetcher pageFetcher = new PageFetcher(config);
+			RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+			RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+			CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+			controller.setCustomData(domain.trim());
+			controller.addSeed(domain.trim());
+
+			//<---configuration done!
+			//Set the crawler with all the configurations and start it
+			PDFCrawler.configure(domain.trim(), storageFolder);
+			controller.start(PDFCrawler.class, numberOfCrawlers);
+
+		}//<---end looping through all the URLs
+
 		Instant end = Instant.now();
 		System.out.println("Completion time: " + Duration.between(start, end));
-	}
-}
+
+	}//end main
+
+}//end class
--- src/com/sustainalytics/ict/PDFCrawler.java
+++ src/com/sustainalytics/ict/PDFCrawler.java
@@ -11,105 +11,110 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.url.WebURL;
 
-/**
- * @author Yasser Ganjisaffar [lastname at gmail dot com]
- */
 
-/*
- * This class shows how you can crawl images on the web and store them in a
- * folder. This is just for demonstration purposes and doesn't scale for large
- * number of images. For crawling millions of images you would need to store
- * downloaded images in a hierarchy of folders
+/**
+ * @author Sustainalytics
+ * @version 1.1 April 20 2015
+ * 
+ * This class shows how you can crawl PDFs on the web and store them in a
+ * folder. 
  */
 public class PDFCrawler extends WebCrawler {
-	private static final Pattern filters = Pattern.compile(".*\\.(bmp|gif|jpe?g|png|tiff?|ico|xaml|pict|rif|pptx?|ps" +
+	//Where we should not go crawling
+	private static final Pattern filters = Pattern.compile(".*\\.(bmp|ashx|gif|jpe?g|png|tiff?|ico|xaml|pict|rif|pptx?|ps" +
 			"|mid|mp2|mp3|mp4|wav|wma|au|aiff|flac|ogg|3gp|aac|amr|au|vox" +
 			"|avi|mov|mpe?g|ra?m|m4v|smil|wm?v|swf|aaf|asf|flv|mkv" +
 			"|zip|rar|gz|7z|aac|ace|alz|apk|arc|arj|dmg|jar|lzip|lha|css|js|bmp|gif|jpe?g|png|tiff?)" +
 			"(\\?.*)?$"); // For url Query parts ( URL?q=... )
-
+	//Target patterns for PDFs
 	private static final Pattern pdfPatterns = Pattern.compile(".*(\\.(pdf?))$");
-
+	//Some other parameters
 	private static File storageFolder;
 	private static String crawlDomain = "";
 	private static File folder;
-
+	/**
+	 * Method to set the configuration parameters of the crawler
+	 * @param domain is the company url to start with
+	 * @param storageFolderName is a string shows where to store the downloaded pdfs
+	 */
 	public static void configure(String domain, String storageFolderName) {
+		
 		PDFCrawler.crawlDomain = domain;
-
 		storageFolder = new File(storageFolderName);
+		
 		if (!storageFolder.exists()) {
 			storageFolder.mkdirs();
 		}
 		
-		folder = new File(storageFolder.getAbsolutePath() + "/" + crawlDomain.substring(7));
+		folder = new File(storageFolder.getAbsolutePath() + "/" + crawlDomain.substring(7)); //get rid of the "HTTP://" part. The folders name will be the company url
+		
 		if (!folder.exists()) {
 			folder.mkdirs();
 		}
-	}
+	}//end method to configure the crawler
+	
+	/**
+	 * Method that overrides the crawler's shouldVisit() method
+	 */
 	@Override
 	public boolean shouldVisit(Page page, WebURL url) {
-		//System.out.println("I am in shouldvisit");
-		//String href = url.getURL().toLowerCase(); 
-		//return true;
-		/*
-		if (!href.startsWith(crawlDomain)) {
-			return false;
-		}
-		
-		if (filters.matcher(href).matches()) {
-			return false;
-		}
-
-		if (pdfPatterns.matcher(href).matches()) {
-			return true;
-		}
-		
-		return false;*/
-		
-		//return href.startsWith(crawlDomain) && pdfPatterns.matcher(href).matches();
-		//return (href.startsWith(crawlDomain) &&  pdfPatterns.matcher(href).matches());
-		//return href.startsWith(this.getMyController().getCustomData().toString()) && pdfPatterns.matcher(href).matches();
 		
 		String href = url.getURL().toLowerCase();
+		
+		/*
+		 *Do not crawl pages that contain the filter RegExes
+		 */
 	    if (filters.matcher(href).matches()) {
 	      return false;
 	    }
-
-	    if (pdfPatterns.matcher(href).matches()) {
-	      return true;
-	    }
-
+	    /*
+		 *Do not crawl pages that are outside of the domain name
+		 */
 	    if (href.startsWith(crawlDomain)) {
 	        return true;
 	      }
+	    /*
+		 *PDFs? Perfect!
+		 */
+	    if (pdfPatterns.matcher(href).matches()) {
+	      return true;
+	    }   
 	    
+	    /*
+		 *No if condition met? Then do not crawl!
+		 */
 	    return false;
-	}
+	}//end overridden shouldVisit() method
 
+	/**
+	 * Visit Method. visit a page and if PDF is found, download and store. Job done! 
+	 */
 	@Override
 	public void visit(Page page) {
+		
 		String url = page.getWebURL().getURL();
-	
+		/*
+		 *Do not crawl pages that do not have PDFs
+		 */
+		
 		if (!pdfPatterns.matcher(url).matches()) {
-			System.out.println("I am in " + url);
-			System.out.println("No match. Leaving.");
+			System.out.println("I am in " +  url + " and leaving.");
 			return;
 		}
-		System.err.println("I am in " + url + " and found something of interest.");
-		// get a unique name for storing this image
+		System.err.println("Yay! Found something at " + url);
+		// Naming the PDF file
 		String extension = url.substring(url.lastIndexOf("."));
 		String hashedName = UUID.randomUUID().toString() + extension;
-		//String path = page.getWebURL().getPath() + extension;
-
-		// store image
 		String filename = folder.getAbsolutePath()+ "/" + hashedName;
-		//String filename = storageFolder.getAbsolutePath() + "/" + path;
+		
+		// store PDF--->
 		try {
 			Files.write(page.getContentData(), new File(filename));
 		} catch (IOException iox) {
-			logger.error("Failed to write file: " + filename, iox);
+			System.out.println("Error storing PDFs");
 		}
-	}
+		//<--- Done storing PDF!
+		
+	}//end overridden visit() method
+	
 }//end class
-
