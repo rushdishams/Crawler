c56317a07e1ac955c3ba5a25b64c41ca5bcd63cc
Rushdi Shams <rushdecoder@gmail.com>
2015-04-29 15:17:28 -0400

    v3.0: CDN for PDFs, html download based on keywords

--- src/com/sustainalytics/ict/PDFCrawler.java
+++ src/com/sustainalytics/ict/PDFCrawler.java
@@ -2,9 +2,9 @@ package com.sustainalytics.ict;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.io.Files;
@@ -17,16 +17,22 @@ import edu.uci.ics.crawler4j.url.WebURL;
 
 /**
  * @author Sustainalytics
- * @version 2.0 April 27 2015
+ * @version 3.0 April 29 2015
  * 
  * This class shows how you can crawl PDFs on the web and store them in a
  * folder. Also the program crawls and downloads html pages that contain some specific terms.
+ * This version also has significant differences than the previous versions.
+ * (1) PDF and HTML pages are now saved based on their original names
+ * (2) If the name contains special characters that are not allowed in Windows file system then the program has
+ * mechanism to resolve it.
+ * (3)If the file already exists in the directory, don't download; otherwise download. This works as a Change
+ * Detection and Notification (CDN) for PDFs
  */
 public class PDFCrawler extends WebCrawler {
 	//-------------------------------------------------------------------------------------------------------------------
 	//Variable section
 	//-------------------------------------------------------------------------------------------------------------------
-	
+
 	//Where we should not go crawling
 	private static final Pattern filters = Pattern.compile(".*\\.(bmp|ashx|gif|jpe?g|png|tiff?|ico|xaml|pict|rif|pptx?|ps" +
 			"|mid|mp2|mp3|mp4|wav|wma|au|aiff|flac|ogg|3gp|aac|amr|au|vox" +
@@ -39,21 +45,8 @@ public class PDFCrawler extends WebCrawler {
 	private static File storageFolder;
 	private static String crawlDomain = "";
 	private static File folder;
-
-	//Regex patterns for 12 different terms to be found on web pages...
-	private static final Pattern regEx1 = Pattern.compile(".*sust.*"),
-			regEx2 = Pattern.compile("csr.*"),
-			regEx3 = Pattern.compile(".*report.*"),
-			regEx4 = Pattern.compile(".*responsibility.*"),
-			regEx5 = Pattern.compile(".*social.*"),
-			regEx6 = Pattern.compile(".*society.*"),
-			regEx7 = Pattern.compile(".*water.*"),
-			regEx8 = Pattern.compile(".*energy.*"),
-			regEx9 = Pattern.compile(".*community.*"),
-			regEx10 = Pattern.compile(".*human rights.*"),
-			regEx11 = Pattern.compile(".*child labor.*"),
-			regEx12 = Pattern.compile(".*suppl.*")
-	;
+	//For naming unnamed html and pdf. It was not used in V3.0 but we have plans to work on this later
+	private int htmlSequence = 0, pdfSequence = 0;
 
 	//-------------------------------------------------------------------------------------------------------------------
 	//Method Section
@@ -122,51 +115,64 @@ public class PDFCrawler extends WebCrawler {
 
 		//if the parsed data is HTML, let's check if it contains certain terms of interest--->
 		if (page.getParseData() instanceof HtmlParseData) {
-			
+
 			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 			String text = htmlParseData.getText();
-			
+
 			//Conditions to match a few terms--->
-			if(regEx1.matcher(text).matches() || regEx2.matcher(text).matches() || regEx3.matcher(text).matches() || regEx4.matcher(text).matches() || regEx5.matcher(text).matches() || regEx6.matcher(text).matches() || regEx7.matcher(text).matches() || regEx8.matcher(text).matches() || regEx9.matcher(text).matches() || regEx10.matcher(text).matches() || regEx11.matcher(text).matches() || regEx12.matcher(text).matches()){
-				System.out.println("-- Regular Expression Matched, Storing HTML --");
-				
+			if(StringUtils.containsIgnoreCase(text, "sust") || StringUtils.containsIgnoreCase(text, "csr") || StringUtils.containsIgnoreCase(text, "report") || StringUtils.containsIgnoreCase(text, "responsibility") || StringUtils.containsIgnoreCase(text, "social") || StringUtils.containsIgnoreCase(text, "society")|| StringUtils.containsIgnoreCase(text, "energy") || StringUtils.containsIgnoreCase(text, "community") || StringUtils.containsIgnoreCase(text, "human rights") || StringUtils.containsIgnoreCase(text, "child labor") || StringUtils.containsIgnoreCase(text, "sust") || StringUtils.containsIgnoreCase(text, "sppl")){
+				//System.out.println("-- Regular Expression Matched, Storing HTML --");
+
 				//Storing HTML--->
-				String hashedName = UUID.randomUUID().toString();
+				String hashedName = FilenameUtils.getBaseName(url).replaceAll("[^a-zA-Z0-9.-]", ""); //getting rid of characters not allowed in Windows file names
 				String filename = folder.getAbsolutePath() + "/" + hashedName + ".html";
-				try {
-					Files.write(page.getContentData(), new File(filename));
-				} catch (IOException iox) {
-					System.out.println("Error storing HTMLs");
-				}
+				File output = new File(filename); //output file
+				//if the file does not exist, it is a new file. Download --->
+				if (!output.exists()){
+					
+					System.out.println("---A new file is found " + filename + " ---");
+					try {
+						Files.write(page.getContentData(), new File(filename));
+					} catch (IOException iox) {
+						System.out.println("Error storing HTMLs");
+					}
+					
+				}//<--- the html is new
+
 				//<---HTML storing done
-			
+
 			}//<---selection of webpage to download is done
-			
+
 		}//<---yes, the data was HTML
-		
+
 		/*
 		 *Return if there is no PDF
 		 */
 		if (!pdfPatterns.matcher(url).matches()) {
-			
-			System.out.println("I am in " +  url + " and leaving.");
+
 			return;
-			
+
 		}
-		
-		System.out.println("--- Found something at " + url + " ---");
-		
+
 		// Naming the PDF file
 		String extension = url.substring(url.lastIndexOf("."));
-		String hashedName = UUID.randomUUID().toString() + extension;
+		String hashedName = FilenameUtils.getBaseName(url).replaceAll("[^a-zA-Z0-9.-]", "") + extension; //removing characters not allowed in Windows file system
 		String filename = folder.getAbsolutePath() + "/" + hashedName;
-
-		// store PDF--->
-		try {
-			Files.write(page.getContentData(), new File(filename));
-		} catch (IOException iox) {
-			System.out.println("Error storing PDFs");
-		}
+		File output = new File(filename); //output file
+		//if the file is new--->
+		if(!output.exists()){
+		
+			System.out.println("---A new file is found " + filename + " ---");
+			
+			// store PDF--->
+			try {
+				Files.write(page.getContentData(), new File(filename));
+			} catch (IOException iox) {
+				System.out.println("Error storing PDFs");
+			}
+		
+		}//<--- the file was new
+		
 		//<--- Done storing PDF!
 
 	}//end overridden visit() method
