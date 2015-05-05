25c78fda9ac0a62899a820bff980496c93ea19e3
rushdishams <rushdecoder@gmail.com>
2015-05-05 15:29:38 +0000

    PDFCrawlerv3.1, PDFCrawlControllerv1.1

--- src/com/sustainalytics/ict/PDFCrawlController.java
+++ src/com/sustainalytics/ict/PDFCrawlController.java
@@ -14,9 +14,11 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 /**
- * The controller class for the crawler. It has a main method that reads all the company URLs
- * from a source text file and crawls them one by one. The method configures the crawler parameters. Also
- * the method keeps track of the time taken by the crawler
+ * The controller class for the crawler. It has a main method that reads all the
+ * company URLs from a source text file and crawls them one by one. The method
+ * configures the crawler parameters. Also the method keeps track of the time
+ * taken by the crawler
+ * 
  * @author Sustainalytics
  * @version 1.1 April 20 2015
  *
@@ -26,60 +28,95 @@ public class PDFCrawlController {
 
 	public static void main(String[] args) throws Exception {
 		/*
-		 * Configuration storage, no. of crawlers to start with, storage folder for the downloaded PDFs
+		 * Configuration storage, no. of crawlers to start with, storage folder
+		 * for the downloaded PDFs
 		 */
-		String rootFolder = "crawler4jStorage";
+		String rootFolder = "Crawler4jStorage";
 		int numberOfCrawlers = 10;
-		String storageFolder = "pdfstorage";
+		String storageFolder = "apple-to-apple-storage";
 
-
-		//Apache Commons IO usage for getting the urls from a source text file --->
+		// Apache Commons IO usage for getting the urls from a source text file
+		// --->
 		File urlFile = new File("input/urls-test.txt");
 		String url = "";
 		try {
 			url = FileUtils.readFileToString(urlFile);
 
 		} catch (IOException e) {
-			System.out
-			.println("Error to read urls from text file");
+			System.out.println("Error to read urls from text file");
 			e.printStackTrace();
 		}
 
 		String[] urls = url.trim().split("\n");
-		//<--- url reading done
-		Instant start = Instant.now(); //start timer
+		// <--- url reading done
+		Instant start = Instant.now(); // start timer
 
-		//for each url--->
-		for (String domain: urls){	
+		// for each url--->
+		for (int i = 0; i < urls.length - 1; i += 2) {
 
-			//Let's configure the crawler--->
-			CrawlConfig config = new CrawlConfig();
-			config.setCrawlStorageFolder(rootFolder); //configuration folder
-			config.setIncludeBinaryContentInCrawling(true); //PDFs are binary contents
-			config.setPolitenessDelay(1); //We are very unpolite. In miliseconds
-			config.setMaxDepthOfCrawling(10); //Infinite depth
-			config.setMaxPagesToFetch(-1); //Infite fetching of pges
-			config.setIncludeHttpsPages(true); //We move to secured http as well
-			config.setMaxDownloadSize(20000000); //Quite a huge file size. In bytes
+			// Let's configure the crawler--->
+			CrawlConfig config1 = new CrawlConfig();
+			CrawlConfig config2 = new CrawlConfig();
+			
+			config1.setCrawlStorageFolder(rootFolder + "/crawler1"); // configuration folder
+			config2.setCrawlStorageFolder(rootFolder + "/crawler2"); // configuration folder
+			
+			config1.setIncludeBinaryContentInCrawling(true); // PDFs are binary
+															// contents
+			config2.setIncludeBinaryContentInCrawling(true); // PDFs are binary
+			// contents
+			config1.setPolitenessDelay(1000); // We are very unpolite. In miliseconds
+			config2.setPolitenessDelay(1000); // We are very unpolite. In miliseconds
+			
+			config1.setMaxDepthOfCrawling(10); // Infinite depth
+			config2.setMaxDepthOfCrawling(10); // Infinite depth
+			
+			config1.setMaxPagesToFetch(-1); // Infite fetching of pges
+			config2.setMaxPagesToFetch(-1); // Infite fetching of pges
+			
+			config1.setIncludeHttpsPages(true); // We move to secured http as
+												// well
+			config2.setIncludeHttpsPages(true); // We move to secured http as
+			// well
 
-			PageFetcher pageFetcher = new PageFetcher(config);
+			config1.setMaxDownloadSize(20000000); // Quite a huge file size. In
+													// bytes
+			config2.setMaxDownloadSize(20000000); // Quite a huge file size. In
+			// bytes
+
+			PageFetcher pageFetcher1 = new PageFetcher(config1);
+			PageFetcher pageFetcher2 = new PageFetcher(config2);
+			
 			RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-			RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-			CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+			RobotstxtServer robotstxtServer = new RobotstxtServer(
+					robotstxtConfig, pageFetcher1);
+			CrawlController controller1 = new CrawlController(config1,
+					pageFetcher1, robotstxtServer);
+			CrawlController controller2 = new CrawlController(config2,
+					pageFetcher2, robotstxtServer);
 
-			controller.setCustomData(domain.trim());
-			controller.addSeed(domain.trim());
+			controller1.setCustomData(urls[i].trim());
+			controller1.addSeed(urls[i].trim());
+			
+			controller2.setCustomData(urls[i + 1].trim());
+			controller2.addSeed(urls[i + 1].trim());
+			//System.out.println("=== " + domain.trim() + " ===");
+			String[] domain = {urls[i], urls[i+1]};
+			// <---configuration done!
+			// Set the crawler with all the configurations and start it
+			PDFCrawler.configure(domain, storageFolder);
+			
+			controller1.startNonBlocking(PDFCrawler.class, numberOfCrawlers);
+			controller2.startNonBlocking(PDFCrawler.class, numberOfCrawlers);
+			
+			controller1.waitUntilFinish();
+			controller2.waitUntilFinish();
 
-			//<---configuration done!
-			//Set the crawler with all the configurations and start it
-			PDFCrawler.configure(domain.trim(), storageFolder);
-			controller.start(PDFCrawler.class, numberOfCrawlers);
-
-		}//<---end looping through all the URLs
+		}// <---end looping through all the URLs
 
 		Instant end = Instant.now();
 		System.out.println("Completion time: " + Duration.between(start, end));
 
-	}//end main
+	}// end main
 
-}//end class
+}// end class
--- src/com/sustainalytics/ict/PDFCrawler.java
+++ src/com/sustainalytics/ict/PDFCrawler.java
@@ -14,167 +14,245 @@ import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
-
 /**
  * @author Sustainalytics
- * @version 3.0 April 29 2015
+ * @version 3.1 May 01 2015
  * 
- * This class shows how you can crawl PDFs on the web and store them in a
- * folder. Also the program crawls and downloads html pages that contain some specific terms.
- * This version also has significant differences than the previous versions.
- * (1) PDF and HTML pages are now saved based on their original names
- * (2) If the name contains special characters that are not allowed in Windows file system then the program has
- * mechanism to resolve it.
- * (3)If the file already exists in the directory, don't download; otherwise download. This works as a Change
- * Detection and Notification (CDN) for PDFs
+ *          This class shows how you can crawl PDFs on the web and store them in
+ *          a folder. Also the program crawls and downloads html pages that
+ *          contain some specific terms. Changes since v3.0: (1) Multiple
+ *          crawler runs at PDFCrawlController.java. So, to accommodate them,
+ *          changes in shouldVisit() and visit() method. Also, no more single
+ *          domain variable; we needed an array of domain variables. (2)
+ *          shouldVisit () method is changed. All possible domains are checked.
+ *          May cause overload. Any optimization will be added to next version
+ *          (3) visit() method is made synchronized because the pdfs and htmls
+ *          are stored in respective websites' folders. If this is not made
+ *          synchronized, the information about which file to store where will
+ *          be lost
  */
 public class PDFCrawler extends WebCrawler {
-	//-------------------------------------------------------------------------------------------------------------------
-	//Variable section
-	//-------------------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------------------
+	// Variable section
+	// -------------------------------------------------------------------------------------------------------------------
 
-	//Where we should not go crawling
-	private static final Pattern filters = Pattern.compile(".*\\.(bmp|ashx|gif|jpe?g|png|tiff?|ico|xaml|pict|rif|pptx?|ps" +
-			"|mid|mp2|mp3|mp4|wav|wma|au|aiff|flac|ogg|3gp|aac|amr|au|vox" +
-			"|avi|mov|mpe?g|ra?m|m4v|smil|wm?v|swf|aaf|asf|flv|mkv" +
-			"|zip|rar|gz|7z|aac|ace|alz|apk|arc|arj|dmg|jar|lzip|lha|css|js|bmp|gif|jpe?g|png|tiff?)" +
-			"(\\?.*)?$"); // For url Query parts ( URL?q=... )
-	//Target patterns for PDFs
-	private static final Pattern pdfPatterns = Pattern.compile(".*(\\.(pdf?))$");
-	//Some other parameters
+	// Where we should not go crawling
+	private static final Pattern filters = Pattern
+			.compile(".*\\.(bmp|ashx|gif|jpe?g|png|tiff?|ico|xaml|pict|rif|pptx?|ps"
+					+ "|mid|mp2|mp3|mp4|wav|wma|au|aiff|flac|ogg|3gp|aac|amr|au|vox"
+					+ "|avi|mov|mpe?g|ra?m|m4v|smil|wm?v|swf|aaf|asf|flv|mkv"
+					+ "|zip|rar|gz|7z|aac|ace|alz|apk|arc|arj|dmg|jar|lzip|lha|css|js|bmp|gif|jpe?g|png|tiff?)"
+					+ "(\\?.*)?$"); // For url Query parts ( URL?q=... )
+	// Target patterns for PDFs
+	private static final Pattern pdfPatterns = Pattern
+			.compile(".*(\\.(pdf?))$");
+	// Some other parameters
 	private static File storageFolder;
-	private static String crawlDomain = "";
-	private static File folder;
-	//For naming unnamed html and pdf. It was not used in V3.0 but we have plans to work on this later
+	private static File[] folder;
+	private static String[] domain;
+	// For naming unnamed html and pdf. It was not used in V3.0 but we have
+	// plans to work on this later
 	private int htmlSequence = 0, pdfSequence = 0;
+	private int currentFolder;
 
-	//-------------------------------------------------------------------------------------------------------------------
-	//Method Section
-	//-------------------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------------------
+	// Method Section
+	// -------------------------------------------------------------------------------------------------------------------
 	/**
 	 * Method to set the configuration parameters of the crawler
-	 * @param domain is the company url to start with
-	 * @param storageFolderName is a string shows where to store the downloaded pdfs
+	 * 
+	 * @param domain
+	 *            is the company url to start with
+	 * @param storageFolderName
+	 *            is a string shows where to store the downloaded pdfs
 	 */
-	public static void configure(String domain, String storageFolderName) {
+	public static void configure(String[] domain, String storageFolderName) {
 
-		PDFCrawler.crawlDomain = domain;
+		// PDFCrawler.crawlDomain = domain;
+		PDFCrawler.domain = domain;
 		storageFolder = new File(storageFolderName);
 
 		if (!storageFolder.exists()) {
 			storageFolder.mkdirs();
 		}
 
-		folder = new File(storageFolder.getAbsolutePath() + "/" + crawlDomain.substring(7)); //get rid of the "HTTP://" part. The folders name will be the company url
+		folder = new File[PDFCrawler.domain.length];
+		for (int i = 0; i < PDFCrawler.domain.length; i++) {
+			String crawlDomain = PDFCrawler.domain[i];
+			folder[i] = new File(storageFolder.getAbsolutePath() + "/"
+					+ crawlDomain.substring(7)); // get rid of the "HTTP://"
+													// part.
+			// The folders name will be the
+			// company url
 
-		if (!folder.exists()) {
-			folder.mkdirs();
+			if (!folder[i].exists()) {
+				folder[i].mkdirs();
+			}
 		}
-	}//end method to configure the crawler
+	}// end method to configure the crawler
 
 	/**
 	 * Method that overrides the crawler's shouldVisit() method
 	 */
 	@Override
-	public boolean shouldVisit(Page page, WebURL url) {
+	public synchronized boolean shouldVisit(Page page, WebURL url) {
 
 		String href = url.getURL().toLowerCase();
 
 		/*
-		 *Do not crawl pages that contain the filter RegExes
+		 * Do not crawl pages that contain the filter RegExes
 		 */
 		if (filters.matcher(href).matches()) {
 			return false;
 		}
 		/*
-		 *Do not crawl pages that are outside of the domain name
+		 * Do not crawl pages that are outside of the domain name
 		 */
-		if (href.startsWith(crawlDomain)) {
-			return true;
+		for (int i = 0; i < PDFCrawler.domain.length; i++) {
+
+			if (href.startsWith(PDFCrawler.domain[i])) {
+
+				return true;
+
+			}
 		}
 		/*
-		 *PDFs? Perfect!
+		 * PDFs? Perfect!
 		 */
 		if (pdfPatterns.matcher(href).matches()) {
 			return true;
-		}   
+		}
 
 		/*
-		 *No if condition met? Then do not crawl!
+		 * No if condition met? Then do not crawl!
 		 */
 		return false;
-	}//end overridden shouldVisit() method
+	}// end overridden shouldVisit() method
 
 	/**
-	 * Visit Method. visit a page and if PDF is found, download and store. Job done! 
+	 * Visit Method. visit a page and if PDF is found, download and store. Job
+	 * done!
 	 */
 	@Override
-	public void visit(Page page) {
+	public synchronized void visit(Page page) {
 
 		String url = page.getWebURL().getURL();
+		// parent url is used because sometimes a.com has pdf links that are on
+		// b.com
+		String parentUrl = page.getWebURL().getParentUrl();
+		// System.out.println("--- " + url + " ---");
+		String baseName = "", fileName = "";
+		File output = null;
 
-		//if the parsed data is HTML, let's check if it contains certain terms of interest--->
+		// this is where may be next versions should concentrate. for this
+		// segment of code, this method is synchronized --->
+		for (int i = 0; i < PDFCrawler.domain.length; i++) {
+
+			if (url.startsWith(PDFCrawler.domain[i])
+					|| parentUrl.startsWith(PDFCrawler.domain[i])) {
+
+				currentFolder = i;
+				break;
+
+			}
+
+		}//<--- that block of code ends here.
+
+		// if the parsed data is HTML, let's check if it contains certain terms
+		// of interest--->
 		if (page.getParseData() instanceof HtmlParseData) {
 
 			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 			String text = htmlParseData.getText();
 
-			//Conditions to match a few terms--->
-			if(StringUtils.containsIgnoreCase(text, "sust") || StringUtils.containsIgnoreCase(text, "csr") || StringUtils.containsIgnoreCase(text, "report") || StringUtils.containsIgnoreCase(text, "responsibility") || StringUtils.containsIgnoreCase(text, "social") || StringUtils.containsIgnoreCase(text, "society")|| StringUtils.containsIgnoreCase(text, "energy") || StringUtils.containsIgnoreCase(text, "community") || StringUtils.containsIgnoreCase(text, "human rights") || StringUtils.containsIgnoreCase(text, "child labor") || StringUtils.containsIgnoreCase(text, "sust") || StringUtils.containsIgnoreCase(text, "sppl")){
-				//System.out.println("-- Regular Expression Matched, Storing HTML --");
+			// System.out.println(text);
 
-				//Storing HTML--->
-				String hashedName = FilenameUtils.getBaseName(url).replaceAll("[^a-zA-Z0-9.-]", ""); //getting rid of characters not allowed in Windows file names
-				String filename = folder.getAbsolutePath() + "/" + hashedName + ".html";
-				File output = new File(filename); //output file
-				//if the file does not exist, it is a new file. Download --->
-				if (!output.exists()){
-					
-					System.out.println("---A new file is found " + filename + " ---");
-					try {
-						Files.write(page.getContentData(), new File(filename));
-					} catch (IOException iox) {
-						System.out.println("Error storing HTMLs");
-					}
-					
-				}//<--- the html is new
+			// We have something in our HTML--->
+			if (text.length() > 0) {
 
-				//<---HTML storing done
+				// Conditions to match a few terms--->
+				if (StringUtils.containsIgnoreCase(text, "sust")
+						|| StringUtils.containsIgnoreCase(text, "csr")
+						|| StringUtils.containsIgnoreCase(text, "report")
+						|| StringUtils.containsIgnoreCase(text,
+								"responsibility")
+						|| StringUtils.containsIgnoreCase(text, "social")
+						|| StringUtils.containsIgnoreCase(text, "society")
+						|| StringUtils.containsIgnoreCase(text, "energy")
+						|| StringUtils.containsIgnoreCase(text, "community")
+						|| StringUtils.containsIgnoreCase(text, "human rights")
+						|| StringUtils.containsIgnoreCase(text, "child labor")
+						|| StringUtils.containsIgnoreCase(text, "sust")
+						|| StringUtils.containsIgnoreCase(text, "sppl")) {
+					System.out.println("-- Current Folder -- " + currentFolder
+							+ " --- Domain --- " + url);
 
-			}//<---selection of webpage to download is done
+					// Storing HTML--->
+					baseName = FilenameUtils.getBaseName(url).replaceAll(
+							"[^a-zA-Z0-9.-]", ""); // getting rid of characters
+													// not
+					// allowed in Windows file names
+					fileName = folder[currentFolder].getAbsolutePath() + "/"
+							+ baseName + ".html";
+					output = new File(fileName); // output file
+					// if the file does not exist, it is a new file. Download
+					// --->
+					if (!output.exists()) {
 
-		}//<---yes, the data was HTML
+						System.out.println("---A new file is found " + fileName
+								+ " ---");
+						try {
+							Files.write(page.getContentData(), new File(
+									fileName));
+						} catch (IOException iox) {
+							System.out.println("Error storing HTMLs");
+						}
+
+					}// <--- the html is new
+
+					// <---HTML storing done
+
+				}// <---selection of webpage to download is done
+
+			}// <--- the HTML is not empty!
+
+		}// <---yes, the data was HTML
 
 		/*
-		 *Return if there is no PDF
+		 * Return if there is no PDF
 		 */
 		if (!pdfPatterns.matcher(url).matches()) {
 
 			return;
 
 		}
+		System.out.println("-- Current Folder -- " + currentFolder
+				+ " --- Domain --- " + url);
 
 		// Naming the PDF file
 		String extension = url.substring(url.lastIndexOf("."));
-		String hashedName = FilenameUtils.getBaseName(url).replaceAll("[^a-zA-Z0-9.-]", "") + extension; //removing characters not allowed in Windows file system
-		String filename = folder.getAbsolutePath() + "/" + hashedName;
-		File output = new File(filename); //output file
-		//if the file is new--->
-		if(!output.exists()){
-		
-			System.out.println("---A new file is found " + filename + " ---");
-			
+		baseName = FilenameUtils.getBaseName(url).replaceAll("[^a-zA-Z0-9.-]",
+				"")
+				+ extension; // removing characters not allowed in Windows file
+		// system
+		fileName = folder[currentFolder].getAbsolutePath() + "/" + baseName;
+		output = new File(fileName); // output file
+		// if the file is new--->
+		if (!output.exists()) {
+
+			System.out.println("---A new file is found " + fileName + " ---");
+
 			// store PDF--->
 			try {
-				Files.write(page.getContentData(), new File(filename));
+				Files.write(page.getContentData(), new File(fileName));
 			} catch (IOException iox) {
 				System.out.println("Error storing PDFs");
 			}
-		
-		}//<--- the file was new
-		
-		//<--- Done storing PDF!
 
-	}//end overridden visit() method
+		}// <--- the file was new
 
-}//end class
+		// <--- Done storing PDF!
+
+	}// end overridden visit() method
+
+}// end class
